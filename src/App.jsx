69059eee7c33d31f57aba497d4d96723299c64d6
habibulloh004 <habibulloh4080@gmail.com>
2024-03-17 20:25:07 +0500

    success2

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,6 @@ function App() {
 
   return (
     <main className="flex-container">
-      <button onClick={() => setAudioPlayed(true)}>click</button>
       {!login && (
         <div className="loginCont">
           <input
